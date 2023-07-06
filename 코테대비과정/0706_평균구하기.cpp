f3e9d9b31335cad79e65015bdcd44afe94c83926
objectio <objection@kakao.com>
2023-07-06 13:40:07 +0900

    [MODIFY] Add category folders & upload 평균구하기

--- 코테대비과정/0706_평균구하기.cpp
+++ 코테대비과정/0706_평균구하기.cpp
@@ -6,14 +6,11 @@ int main()
    	int N = 0;
    	int A[1000];
    	cin >> N;
-   	for (int i = 0; i < N; i++){
-      cin >> A[i];
-   	}
-
-  	int sum = 0;
+	int sum = 0;
 	int max = 0;
-   	
-	for (int i = 0; i < N; i++) {
+
+   	for (int i = 0; i < N; i++){
+    	cin >> A[i];
 		if (max < A[i]) max = A[i];
 		sum += A[i];
    	}
